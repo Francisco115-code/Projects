85f691b9646f2a3c99caaf74eebbe2334eb80492
Francisco115-code <78924983+Francisco115-code@users.noreply.github.com>
2023-03-07 13:21:49 -0600

    Backend Framework (Full Stack Coding Challenge)
    Yaml files, Php files for 3 databases images and containers of Employee, Login, and Registration.

---
+++ webapp.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "localhost:8080/employee";
+$dbUsername = "root";
+$dbPassword = "password";
+$dbName = "employee";
+
+$conn = mysqli_connect( $dbServername , $dbUsername , $dbPassword , $dbName );
+
+?>
